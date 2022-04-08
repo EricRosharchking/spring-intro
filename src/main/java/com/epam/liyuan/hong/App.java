65b12a854543f114a4e9ad42a220eadd61bb6688
Liyuan Hong <liyuan_hong@epam.com>
2022-04-08 11:05:59 +0800

    Add all beans in configuration.xml

--- src/main/java/com/epam/liyuan/hong/App.java
+++ src/main/java/com/epam/liyuan/hong/App.java
@@ -1,13 +1,22 @@
 package com.epam.liyuan.hong;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.epam.liyuan.hong.dao.ItemDao;
+import com.epam.liyuan.hong.facade.BookingFacade;
+import com.epam.liyuan.hong.service.EventService;
+import com.epam.liyuan.hong.service.TicketService;
+import com.epam.liyuan.hong.service.UserService;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+		
+		EventService e = (EventService) context.getBean("eventService");
+		TicketService t = (TicketService) context.getBean("ticketService");
+		UserService u = (UserService) context.getBean("userService");
+		BookingFacade b = (BookingFacade) context.getBean("bookingImpl");
+		ItemDao i = (ItemDao) context.getBean("itemDao");
+	}
 }
