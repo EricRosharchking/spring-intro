a2bfdc6bb583352460f4e4cd95097ab38856ee41
Liyuan Hong <liyuan_hong@epam.com>
2022-04-09 22:02:53 +0800

    Init Test config, add RepoTest class

---
+++ src/test/java/com/epam/liyuan/hong/RepoTest.java
@@ -0,0 +1,60 @@
+package com.epam.liyuan.hong;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.epam.liyuan.hong.model.Event;
+import com.epam.liyuan.hong.repo.ItemRepo;
+
+@RunWith(SpringRunner.class)
+@PropertySource("classpath:app.properties")
+@ContextConfiguration("classpath:configuration.xml")
+public class RepoTest {
+
+	@Autowired
+	ApplicationContext context;
+	
+	private ItemRepo itemRepo;
+
+	@Value(".")
+	private String classpath;
+
+	@Value("${app.prepareddata.file.event}")
+	private String eventsJson;
+
+	@Test
+	public void testWrite() {
+		Event event = new Event(1L, "test", new Date());
+		Map<Long, Event> map = new HashMap<>();
+		map.put(event.getId(), event);
+		itemRepo = (ItemRepo) context.getBean("itemRepo");
+		assertNotNull(itemRepo);
+		assertTrue(itemRepo.saveEvents(map));
+		try {
+			assertTrue(Files.size(Paths.get("", eventsJson)) > 0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assertTrue(false);
+		}
+	}
+}
